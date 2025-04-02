5440eb28c35c4c75fe76d52ac52210507e6b8cb4
张怡琳 <zhangyilin@shgbit.com>
2025-04-02 22:36:44 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    assetsDir: ''
   },
   plugins: [react()],
 
